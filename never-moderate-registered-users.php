d4acdb4826c35d3a11d9f7facc77359b0c554881
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-31 06:44:41 +0000

    * Update to v2.1.1 * Minor plugin header reformatting * Minor code reformatting (bracing, spacing) * Minor documentation reformatting (spacing, typo) * Change documentation links to wp.org to be https * Note compatibility through WP 4.0+ * Add plugin icon
    git-svn-id: https://plugins.svn.wordpress.org/never-moderate-registered-users/tags/2.1.1@976002 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- never-moderate-registered-users.php
+++ never-moderate-registered-users.php
@@ -1,62 +1,76 @@
 <?php
 /**
+ * Plugin Name: Never Moderate Registered Users
+ * Version:     2.1.1
+ * Plugin URI:  http://coffee2code.com/wp-plugins/never-moderate-registered-users/
+ * Author:      Scott Reilly
+ * Author URI:  http://coffee2code.com/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Description: Never moderate or mark as spam comments made by registered users, regardless of the apparent spamminess of the comment.
+ *
+ * Compatible with WordPress 3.1 through 4.0+.
+ *
+ * =>> Read the accompanying readme.txt file for instructions and documentation.
+ * =>> Also, visit the plugin's homepage for additional information and updates.
+ * =>> Or visit: https://wordpress.org/plugins/never-moderate-registered-users/
+ *
  * @package Never_Moderate_Registered_Users
  * @author Scott Reilly
- * @version 2.0.1
+ * @version 2.1.1
  */
-/*
-Plugin Name: Never Moderate Registered Users
-Version: 2.0.1
-Plugin URI: http://coffee2code.com/wp-plugins/never-moderate-registered-users/
-Author: Scott Reilly
-Author URI: http://coffee2code.com
-Description: Never moderate or mark as spam comments made by registered users, regardless of the apparent spamminess of the comment.
-
-Compatible with WordPress 2.5+, 2.6+, 2.7+, 2.8+, 2.9+, 3.0+, 3.1+.
-
-=>> Read the accompanying readme.txt file for instructions and documentation.
-=>> Also, visit the plugin's homepage for additional information and updates.
-=>> Or visit: http://wordpress.org/extend/plugins/never-moderate-registered-users/
-
-*/
 
 /*
-Copyright (c) 2008-2011 by Scott Reilly (aka coffee2code)
+	Copyright (c) 2008-2014 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-if ( !function_exists( 'c2c_never_moderate_registered_users' ) ) :
+defined( 'ABSPATH' ) or die();
+
+if ( ! function_exists( 'c2c_never_moderate_registered_users' ) ) :
 /**
  * Never moderate comments by registered users.
  *
  * @since 1.0
  *
- * @param int $approved Current approval status for comment
- * @return int If the comment is approved
+ * @param int|string $approved    Current approval status for comment: 0, 1, spam
+ * @param array      $commentdata The comment data
+ * @return int|string             New approval status for comment, either same as incoming or 1
  */
-function c2c_never_moderate_registered_users( $approved ) {
-	global $wpdb, $commentdata;
-	$user_id = isset( $commentdata['user_ID'] ) ? $commentdata['user_ID'] : false;
+function c2c_never_moderate_registered_users( $approved, $commentdata ) {
+	global $wpdb;
+
+	if ( isset( $commentdata['user_ID'] ) ) {
+		$user_id = $commentdata['user_ID'];
+	} elseif ( isset( $commentdata['user_id'] ) ) {
+		$user_id = $commentdata['user_id'];
+	} else {
+		$user_id = false;
+	}
 
 	// If the comment isn't from a registered user, or is already approved, don't change approval status
-	if ( !$user_id || $approved )
+	if ( ! $user_id || 1 == $approved ) {
 		return $approved;
+	}
 
 	$user = new WP_User( $user_id );
+
 	if ( $user ) {
 		$trusted_caps = (array) apply_filters( 'c2c_never_moderate_registered_users_caps', array() );
+
 		if ( empty( $trusted_caps ) ) {
 			$has_cap = true;
 		} else {
@@ -68,14 +82,14 @@ function c2c_never_moderate_registered_users( $approved ) {
 				}
 			}
 		}
-		if ( $has_cap )
+
+		if ( $has_cap ) {
 			$approved = 1;
+		}
 	}
 
 	return $approved;
 }
 endif;
 
-add_filter( 'pre_comment_approved', 'c2c_never_moderate_registered_users', 15 );
-
-?>
+add_filter( 'pre_comment_approved', 'c2c_never_moderate_registered_users', 15, 2 );
